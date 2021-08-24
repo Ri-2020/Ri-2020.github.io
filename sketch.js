498a9d30134e22dc46e1d8e2a00b11683e1e0fab
Rohit Gupta <74013705+Ri-2020@users.noreply.github.com>
2021-08-24 22:56:07 +0530

    Bug Fixes
    Before: if you press & release the mouse outside canvas the ball just released from the constraints
    Now: Only if you press and release the mouse inside the canvas then only happy ball released

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 let ground;
 let boxes = [];
 let bird;
-let world , engine;
+let world, engine;
 let mConstraint;
 let Bodies = Matter.Bodies;
 let World = Matter.World;
@@ -9,11 +9,14 @@ let Engine = Matter.Engine;
 let Mouse = Matter.Mouse;
 let MouseConstraint = Matter.MouseConstraint;
 let slingShot;
+let button;
+let flag = 0
+
 
 
 let dotImg, boxImg, bgImg, grdImg;
 
-function preload(){
+function preload() {
   dotImg = loadImage('Images/dot.png');
   boxImg = loadImage('Images/equals.png')
   bgImg = loadImage('Images/skyBackground.png')
@@ -23,64 +26,73 @@ function preload(){
 
 
 function setup() {
-  const canvas = createCanvas(700 , 400);
+  const canvas = createCanvas(700, 400);
   engine = Engine.create();
   world = engine.world;
 
 
-  ground = new Ground(width/2 , height-20 , width+5 , 40);
-  for(var i=0 ; i<3 ; i++){
-    boxes[i] = new Box(550 , 300-i*75  , 100 ,100);
+  ground = new Ground(width / 2, height - 20, width + 5, 40);
+  for (var i = 0; i < 3; i++) {
+    boxes[i] = new Box(550, 300 - i * 75, 100, 100);
   }
-  bird  = new Bird(150 , 300, 25);
+  bird = new Bird(150, 300, 25);
 
-  slingShot = new SlingShot(150 , 250 , bird.body);
+  slingShot = new SlingShot(150, 250, bird.body);
 
   const cmouse = Mouse.create(canvas.elt)
   cmouse.pixelRatio = pixelDensity()
   const options = {
     mouse: cmouse
   }
-  mConstraint = MouseConstraint.create(engine , options);
-  World.add(world , mConstraint);
+  mConstraint = MouseConstraint.create(engine, options);
+  World.add(world, mConstraint);
   document.querySelector('.hide').style.display = "none";
 }
 
-  function keyPressed(){
-    
-    if(key == " "){
-      World.remove(world , bird.body);
-      bird = new Bird(150 , 300, 25);
-      slingShot = new SlingShot(150 , 250 , bird.body);
+function keyPressed() {
+  
+    if (key == " ") {
+      World.remove(world, bird.body);
+      bird = new Bird(150, 300, 25);
+      slingShot = new SlingShot(150, 250, bird.body);
     }
-    else{
+    else {
       return false;
     }
-  }
+}
 
-function mouseReleased(){
+let inX , inY;
+
+function mousePressed(){
+  inX = mouseX
+  inY = mouseY
+}
+
+function mouseReleased() {
+  if (inX > 0 && inY > 0 && inX < width && inY < height) {
   setTimeout(() => {
     slingShot.fly();
   }, 20);
+}
 }
 
 function draw() {
   background(bgImg);
   Engine.update(engine);
   ground.show();
-  for(var i =0 ; i<boxes.length ; i++){
+  for (var i = 0; i < boxes.length; i++) {
     boxes[i].show();
-    if(boxes[i].body.position.x > 750){
-      World.remove(world , boxes[i]);
-      boxes.splice(i,1);
-      i=i-1;
+    if (boxes[i].body.position.x > 750) {
+      World.remove(world, boxes[i]);
+      boxes.splice(i, 1);
+      i = i - 1;
     }
   }
   var update = document.querySelector(".update");
   update.innerText = boxes.length
 
 
-  if(boxes.length == 0){
+  if (boxes.length == 0) {
     var won = document.querySelector(".title");
     won.innerText = "You Won!!"
 
@@ -89,7 +101,7 @@ function draw() {
     }, 2000);
 
     setTimeout(() => {
-      
+
       won.innerText = "Reloding Game"
     }, 3000);
     setTimeout(() => {
@@ -98,7 +110,7 @@ function draw() {
     }, 3000);
   }
 
-  
+
   bird.show();
   slingShot.show()
 }
