cdfd2e6e30deaaca4ac01367b7b31eee3b6a46a1
Rohit Gupta <74013705+Ri-2020@users.noreply.github.com>
2021-08-25 22:38:43 +0530

    collission property
    collission property

--- Objects/Box.js
+++ Objects/Box.js
@@ -1,6 +1,10 @@
 class Box{
     constructor(x,y,w,h) {
         this.body = Bodies.rectangle(x , y , w , h);
+        this.body.collisionFilter.group  = 0;
+        this.body.collisionFilter.mask  = 2;
+        this.body.collisionFilter.category = 2;
+
         World.add(world , this.body);
         this.w = w;
         this.h = h;
